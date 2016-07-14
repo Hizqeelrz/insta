d7ff54b518020a6a84f81da2563738196dd58b2d
hizqeelrz <hizqeel1214@gmail.com>
2016-07-14 17:21:42 +0500

    bug fixed for name attribute for users :)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714112450) do
+ActiveRecord::Schema.define(version: 20160714121906) do
 
   create_table "pics", force: :cascade do |t|
     t.string   "title"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160714112450) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160714112450) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
